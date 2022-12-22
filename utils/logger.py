62b54e75c621d2ca10c4465206811e148cb88f7f
Dan Nelson <danelson@nwi.net>
2022-12-21 16:00:26 -0800

    added logger and main.py

---
+++ utils/logger.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+
+
+def setup_logger(name=__name__):
+    # create a custom logger
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    # Create handlers
+    c_handler = logging.StreamHandler(sys.stdout)
+    f_handler = logging.FileHandler("./logs.log")
+    
+    # Add formatters
+    c_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    f_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    c_handler.setFormatter(c_format)
+    f_handler.setFormatter(f_format)
+
+    # Add handlers to the logger
+    logger.addHandler(c_handler)
+    logger.addHandler(f_handler)
+
+    return logger
+
+if __name__ == "--main__":
+    logg = setup_logger()
+    logg.info("Test")
